b4619bb0c266e708d4f7de6636f814f8297a1f52
SuperAsneiitor <qiaoqihang97@gmail.com>
2026-04-08 11:13:26 +0800

    Update spilt_design.tcl

--- inn/my_scr/proc/spilt_design.tcl
+++ inn/my_scr/proc/spilt_design.tcl
@@ -152,6 +152,23 @@ proc create_fence_selected_by_box { fence_name fence_box } {
     }
 }
 
+proc create_complex_fence_selected_by_box {fence_name fence_boxList} {
+    set core_llx [dbget top.fplan.coreBox_llx]
+    set core_lly [dbget top.fplan.coreBox_lly]
+
+    # create empty fence
+    createInstGroup $fence_name -fence "$core_llx $core_lly [expr $core_llx + 1]  [expr $core_lly + 1] "
+    
+    # modify fence shape
+    setObjFPlanBoxList Group $fence_name $fence_boxList
+    
+    # add instance to fence
+    foreach inst [dbget selected.name] {
+        addInstToInstGroup $fence_name $inst
+    }
+}
+
+
 
 
 proc create_partition_by_box {partition_name partition_box  create_hinst create_fence} {
@@ -199,11 +216,54 @@ proc create_partition_by_box {partition_name partition_box  create_hinst create_
 }
 
 
+proc create_partition_by_complex_shape {partition_name partition_box_list pdb_dir} {
+    #
+    setLayerPreference node_layer -isVisible 0
+    setLayerPreference term -isVisible 0
+    set timing_defer_mmmc_object_updates true
+
+    
+    # Action 1. >>> select all instances in the box_list
+    deselectAll
+    foreach sub_partition_box $partition_box_list {
+        set partition_box_llx [lindex $sub_partition_box 0]
+        set partition_box_lly [lindex $sub_partition_box 1]
+        set partition_box_urx [lindex $sub_partition_box 2]
+        set partition_box_ury [lindex $sub_partition_box 3]
+
+        select_obj [dbget top.insts {.pt_x>=${partition_box_llx} && .pt_x<=${partition_box_urx} \
+                                  && .pt_y>=${partition_box_lly} && .pt_y<=${partition_box_ury}}]
+    }
+
+    # Action 2. >>> dump place def of seleccted instances
+    defOut -selected ../data/${partition_name}.def
+    
+    # Action 3. >>> judge selected instance number
+    if {[llength [dbget selected]] < 2} {
+        puts "\[Error\]: No instances selected, Please Check your partition_box_list !!!"
+        return
+    }
+    
+    # Action 4. >>> create instance group & fence
+    select_path_from_insts [dbget [dbget selected.cell.isSequential 1 -p2].name]
+    create_complex_fence_selected_by_box $partition_name $partition_box_list
+    catch {createLogicHierarchy -cell ${partition_name}_cell -newHinst $partition_name -objects $partition_name -commit}
 
 
+    # 4.2 create Fence & Module
+    set core_llx [dbget top.fplan.coreBox_llx]
+    set core_lly [dbget top.fplan.coreBox_lly]
+    createFence ${partition_name}   $core_llx $core_lly [expr $core_llx + 1]  [expr $core_lly + 1]
+    setObjFPlanBoxList Module  $partition_name  $partition_box_list
 
 
+    set_analysis_view -update_timing
 
+    # Action 5. >>> split design & save PDB
+    catch {definePartition -hinst $partition_name}
+    catch {deriveTimingBudget -timingCore -timingIlmCore}
+    catch {partition -noInheritPhysical}
 
-
+    savePartition -dir $pdb_dir
+}
 
